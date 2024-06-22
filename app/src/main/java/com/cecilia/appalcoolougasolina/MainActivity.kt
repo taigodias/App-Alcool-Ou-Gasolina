b935409b92fdb3ef0c8d857fdf90084981738782
Cecília Maria <cecilia.msaraujo9@gmail.com>
2024-06-22 13:22:59 -0300

    Initial Commit

---
+++ app/src/main/java/com/cecilia/appalcoolougasolina/MainActivity.kt
@@ -0,0 +1,34 @@
+ package com.cecilia.appalcoolougasolina
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.cecilia.appalcoolougasolina.ui.theme.AppAlcoolOuGasolinaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppAlcoolOuGasolinaTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    App()
+                }
+            }
+        }
+    }
+    @Composable
+    fun App(){
+
+    }
+}
+
