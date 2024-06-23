0d2ca5fa3c039a8784d7c095094609cb075815ff
Cecília Maria <cecilia.msaraujo9@gmail.com>
2024-06-23 19:51:24 -0300

    ajustes finais

--- app/src/main/java/com/cecilia/appalcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/cecilia/appalcoolougasolina/MainActivity.kt
@@ -12,8 +12,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -132,6 +142,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+
     @Preview
     @Composable
     fun AppPreview() {
@@ -140,4 +151,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
--- app/src/main/java/com/cecilia/appalcoolougasolina/ui/theme/Color.kt
+++ app/src/main/java/com/cecilia/appalcoolougasolina/ui/theme/Color.kt
@@ -2,12 +2,10 @@ package com.cecilia.appalcoolougasolina.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val md_theme_light_primary = Color(0xFF6750A4)
-val md_theme_light_onPrimary = Color(0xFFFFFFFF)
-val md_theme_light_primaryContainer = Color(0xFFEADDFF)
-val md_theme_light_onPrimaryContainer = Color(0xFF21005E)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
 
-val md_theme_dark_primary = Color(0xFFD0BCFF)
-val md_theme_dark_onPrimary = Color(0xFF381E72)
-val md_theme_dark_primaryContainer = Color(0xFF4F378B)
-val md_theme_dark_onPrimaryContainer = Color(0xFFEADDFF)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
